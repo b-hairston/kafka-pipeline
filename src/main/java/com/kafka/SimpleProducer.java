5755ef0b7acc885e44b2625d400cd50275d06c4b
b-hairston <bph484@gmail.com>
2024-03-08 00:34:15 -0600

    created producer & consumer, port listening is fucked though

--- src/main/java/com/kafka/SimpleProducer.java
+++ src/main/java/com/kafka/SimpleProducer.java
@@ -3,7 +3,11 @@ import static java.lang.System.*;
 
 import java.util.*;
 
+import org.apache.kafka.clients.admin.Admin;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.serialization.*;
 
 
@@ -20,31 +24,31 @@ public final class SimpleProducer{
     // Define the topic to which we will publish
     final var topic = "getting-started";
     
-
+    // create a topic
+            
     // Define the configuration for the producer using a Map
+    // TODO: sort out the port listening configuration
     final Map<String, Object> config =
             Map.of(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092", // tell the producer where to connect to
             ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, // tell the producer how to serialize the key
             StringSerializer.class.getName(), // use the string serializer provided by Kafka
             ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, // tell the producer how to serialize the value
             StringSerializer.class.getName(), // use the string serializer provided by Kafka
-            ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true); // ensure that we don't push duplicates
+            ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true,
+            ProducerConfig.MAX_BLOCK_MS_CONFIG, 300000); // ensure that we don't push duplicates
 
     try (var producer = new KafkaProducer<String, String>(config)){  // create a new producer (KafkaProducer object)
         while(true){
             final var key = "myKey"; // the key of the record
             final var value = "myValue"; // the value of the record
-            out.format("Publishing record with valeu %s%n", value); // log the record
+            out.format("Publishing record with value %s%n", value); // log the record
 
     
-    final Callback callback = (metadata, exception) -> { // create a callback to log the result
-        if (exception != null){
-            out.format("Published with metadata: %s, exception: %s%n", metadata, exception); // log the metadata and exception
-        } else {
-            out.format("Published record to topic %s partition [%d] @ offset %d%n", // log the metadata
-            metadata.topic(), metadata.partition(), metadata.offset());
-        }
-    };
+    final Callback callback = (metadata, exception) -> {
+        out.format("Published with metadata: %s, error: %s%n",
+        metadata, exception);
+        };
+                
 
     producer.send(new ProducerRecord<>(topic, key, value), callback); // publish the record to the topic
 
